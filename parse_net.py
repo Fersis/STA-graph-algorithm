6a2f39066e24c9cbb8183e47d1f31164eb7ea91a
Fersis <tpjkl0912dhcg@126.com>
2021-10-17 16:58:27 +0800

    Delete test file graph.py, add new main file, add graph class

--- parse_net.py
+++ parse_net.py
@@ -2,25 +2,38 @@ import re
 import networkx as nx
 import matplotlib.pyplot as plt
 
-data_path = 'data/grpout_1/design.net'
-with open(data_path) as f:
-    lines = f.readlines()
 
-nodes = []
-for line in lines:
-    nodes.append(re.search(r'(?P<name>g[p0-9]+) (?P<direction>[ls])', line))
+class Graph:
+    def __init__(self, data_path) -> None:
+        self.data_path = data_path
+        with open(self.data_path) as f:
+            lines = f.readlines()
 
-split_points = []
-for i, node in enumerate(nodes):
-    if (node.group('direction') == 's'):
-        split_points.append(i)
-split_points.append(len(nodes))
+        nodes = []
+        for line in lines:
+            nodes.append(re.search(r'(?P<name>g[p0-9]+) (?P<direction>[ls])',
+                line))
 
-graph = nx.DiGraph()
-for split_index in range(len(split_points) - 1):
-    start = nodes[split_points[split_index]].group('name')
-    for i in range(split_points[split_index] + 1, split_points[split_index + 1]):
-        graph.add_edge(start, nodes[i].group('name'))
+        # Get all indexes of split points
+        split_indexes = []
+        for i, node in enumerate(nodes):
+            if (node.group('direction') == 's'):
+                split_indexes.append(i)
+        # End index should also be a split point
+        split_indexes.append(len(nodes))
 
-nx.draw_kamada_kawai(graph, with_labels=True, node_size=1000)
-plt.show()
+        # Add nodes and edges to the directed graph
+        self.graph = nx.DiGraph()
+        # For example, the split_indexes is [0, 3, 6, 9], itereate over
+        # [0, 3], [3, 6], [6, 9], each one is a group of edges.
+        # In [0, 3], iterate from 1 to 3.
+        for i in range(len(split_indexes) - 1):
+            start = nodes[split_indexes[i]].group('name')
+            for j in range(split_indexes[i] + 1, split_indexes[i + 1]):
+                end = nodes[j].group('name')
+                self.graph.add_edge(start, end)
+
+    def draw(self):
+        nx.draw_kamada_kawai(self.graph, with_labels=True, node_size=1000)
+        plt.show()
+
