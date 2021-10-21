9c3dcf85476af3cc10f4e3e852db2b674542d018
Fersis <tpjkl0912dhcg@126.com>
2021-10-21 20:55:52 +0800

    Use main.ipynb instead of main.py; Add node direction and is_port property; Remove vdd, vss pins

--- parse_net.py
+++ parse_net.py
@@ -1,18 +1,22 @@
 import re
 import networkx as nx
 import matplotlib.pyplot as plt
+from networkx.generators import directed
 
 
 class Graph:
     def __init__(self, data_path) -> None:
         self.data_path = data_path
-        with open(self.data_path) as f:
+
+        # Read design.net
+        net_path = self.data_path + '/design.net'
+        with open(net_path) as f:
             lines = f.readlines()
 
         nodes = []
         for line in lines:
             nodes.append(re.search(r'(?P<name>g[p0-9]+) (?P<direction>[ls])',
-                line))
+                                   line))
 
         # Get all indexes of split points
         split_indexes = []
@@ -32,8 +36,31 @@ class Graph:
             for j in range(split_indexes[i] + 1, split_indexes[i + 1]):
                 end = nodes[j].group('name')
                 self.graph.add_edge(start, end)
+                self._add_node(end, direction='l')
+            self._add_node(start, direction='s')
+
+        # Remove VDD and VSS
+        vdd_vss_nodes = []
+        for node in self.graph:
+            if (self.graph.nodes[node]['direction'] == 's'
+                    and self.graph.nodes[node]['is_port'] == False):
+                vdd_vss_nodes.append(node)
+        self.graph.remove_nodes_from(vdd_vss_nodes)
 
     def draw(self):
         nx.draw_kamada_kawai(self.graph, with_labels=True, node_size=1000)
         plt.show()
 
+    def _add_node(self, name, direction: str):
+        # Add direction property
+        keys = self.graph.nodes[name].keys()
+        if ('direction' not in keys):
+            self.graph.add_node(name, direction=direction)
+        elif (self.graph.nodes[name]['direction'] != direction):
+            self.graph.add_node(name, direction='s/l')
+
+        # Add is_port property
+        if 'p' in name:
+            self.graph.add_node(name, is_port=True)
+        else:
+            self.graph.add_node(name, is_port=False)
