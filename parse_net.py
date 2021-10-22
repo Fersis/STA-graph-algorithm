83283efdd5ef8eebf1454ca30efbb7a03447dd03
Fersis <tpjkl0912dhcg@126.com>
2021-10-22 22:22:54 +0800

    Add ff and clk property; Refactor _add_direction, _add_port method

--- parse_net.py
+++ parse_net.py
@@ -1,7 +1,6 @@
 import re
 import networkx as nx
 import matplotlib.pyplot as plt
-from networkx.generators import directed
 
 
 class Graph:
@@ -36,8 +35,10 @@ class Graph:
             for j in range(split_indexes[i] + 1, split_indexes[i + 1]):
                 end = nodes[j].group('name')
                 self.graph.add_edge(start, end)
-                self._add_node(end, direction='l')
-            self._add_node(start, direction='s')
+                self._add_direction(end, direction='l')
+                self._add_port(end)
+            self._add_direction(start, direction='s')
+            self._add_port(start)
 
         # Remove VDD and VSS
         vdd_vss_nodes = []
@@ -47,20 +48,54 @@ class Graph:
                 vdd_vss_nodes.append(node)
         self.graph.remove_nodes_from(vdd_vss_nodes)
 
+        # Read design.are
+        are_path = self.data_path + '/design.are'
+        with open(are_path) as f:
+            lines = f.readlines()
+
+        for line in lines:
+            match = re.search(
+                r'(?P<name>g[p0-9]+)\s?(?:{(?P<is_ff>ff)?\s?(?P<clk>c\d+)?})?',
+                line)
+            node_name = match.group('name')
+            # Exclude Vdd and Vss
+            if node_name in self.graph:
+                # Add ff property
+                if match.group('is_ff'):
+                    self.graph.add_node(node_name, is_ff=True)
+                else:
+                    self.graph.add_node(node_name, is_ff=False)
+                # Add clk property
+                if match.group('clk'):
+                    self.graph.add_node(node_name, clk=match.group('clk'))
+                else:
+                    self.graph.add_node(node_name, clk=None)
+
     def draw(self):
         nx.draw_kamada_kawai(self.graph, with_labels=True, node_size=1000)
         plt.show()
 
-    def _add_node(self, name, direction: str):
-        # Add direction property
+    def _add_direction(self, name: str, direction: str):
+        """
+        Add direction property
+        """
         keys = self.graph.nodes[name].keys()
         if ('direction' not in keys):
             self.graph.add_node(name, direction=direction)
         elif (self.graph.nodes[name]['direction'] != direction):
             self.graph.add_node(name, direction='s/l')
 
-        # Add is_port property
+    def _add_port(self, name: str):
+        """
+        Add is_port property
+        """
         if 'p' in name:
             self.graph.add_node(name, is_port=True)
         else:
             self.graph.add_node(name, is_port=False)
+
+
+
+data_path2 = 'data/grpout_2'
+graph2 = Graph(data_path=data_path2)
+print(graph2.graph.nodes.data())
