5df08be6709a6dc2686f02ad919be274fd08b672
Fersis <tpjkl0912dhcg@126.com>
2021-11-27 15:10:51 +0800

    Add ClockSource class

--- ta_classes.py
+++ ta_classes.py
@@ -1,8 +1,9 @@
 
 class DFF:
     """Stores it's clock domain and clock source latency
-    
+
     A DFF should have a "ff" property and a clock domain in design.are file."""
+
     def __init__(self, clk: str = ''):
         self.tco = 1.0
         self.clk = clk
@@ -11,19 +12,32 @@ class DFF:
 
 class Port:
     """Store the direction_of_signal
-    
+
     This port class includes "in port" and "out port" and has "p" character
     in name.
     """
+
     def __init__(self, direction_of_signal: str):
         self.direction_of_signal = direction_of_signal
 
 
 class Cell:
     """Stores the cell delay
-    
+
     A cell shouldn't contain any "ff" property or clock domain but only a
     cell name. For example: "g4".
     """
+
     def __init__(self, delay: float = 0.0):
         self.delay = delay
+
+
+class ClockSource:
+    """Stores the clock source name
+
+    A clock source is a port with only a clock domain that is for example
+    "gp0 {c1}."
+    """
+
+    def __init__(self, clk_domain: str):
+        self.clock_domain = clk_domain
