4ddaf30589d4eb68398fe48bedc248e0a835e637
Fersis <tpjkl0912dhcg@126.com>
2021-11-27 15:00:17 +0800

    Add DFF, Port and Cell classes docstring

--- ta_classes.py
+++ ta_classes.py
@@ -1,5 +1,8 @@
 
 class DFF:
+    """Stores it's clock domain and clock source latency
+    
+    A DFF should have a "ff" property and a clock domain in design.are file."""
     def __init__(self, clk: str = ''):
         self.tco = 1.0
         self.clk = clk
@@ -7,10 +10,20 @@ class DFF:
 
 
 class Port:
+    """Store the direction_of_signal
+    
+    This port class includes "in port" and "out port" and has "p" character
+    in name.
+    """
     def __init__(self, direction_of_signal: str):
         self.direction_of_signal = direction_of_signal
 
 
 class Cell:
+    """Stores the cell delay
+    
+    A cell shouldn't contain any "ff" property or clock domain but only a
+    cell name. For example: "g4".
+    """
     def __init__(self, delay: float = 0.0):
         self.delay = delay
