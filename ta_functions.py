f0f77f13386a9c80d7de5ff6fa652db42fa0700e
Fersis <tpjkl0912dhcg@126.com>
2021-12-12 01:12:37 +0800

    Add with loops method

--- ta_functions.py
+++ ta_functions.py
@@ -7,7 +7,7 @@ def get_paths(G: nx.DiGraph, start) -> list:
     
     A recursive method which accepts a start node and returns path starting
     from this start node. It firstly selects a child of start, if this child
-    is DFF or Port, it returns. Else, make this child as start and recusively
+    is DFF or Port, it returns. Else, make this child as start and recursively
     search all children of this child.
     """
     for node in G[start]:
@@ -16,3 +16,23 @@ def get_paths(G: nx.DiGraph, start) -> list:
         else:
             for child in get_paths(G, node):
                 yield [start] + child
+
+
+def get_paths_with_loops(G: nx.DiGraph, start, path_behind):
+    path_behind.append(start)
+    for node in G[start]:
+        if G.out_degree[start] > 1:
+            if node not in path_behind:
+                if G.out_degree[node] == 0:
+                    yield [start, node]
+                else:
+                    for child in get_paths_with_loops(G, node, path_behind):
+                        yield [start] + child
+            else:
+                continue
+        else:
+            if G.out_degree[node] == 0:
+                yield [start, node]
+            else:
+                for child in get_paths_with_loops(G, node, path_behind):
+                    yield [start] + child
