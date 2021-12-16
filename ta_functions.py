2b5b01a0a06acaa3746aa1e394f8c00fca9f1e52
Fersis <tpjkl0912dhcg@126.com>
2021-12-17 00:37:38 +0800

    Optimize get_paths method

--- ta_functions.py
+++ ta_functions.py
@@ -2,19 +2,24 @@ import networkx as nx
 import ta_classes as ta
 
 
-def get_paths(G: nx.DiGraph, start, path_behind: list) -> list:
-    """A generator returns paths
-    
-    A recursive method which accepts a start node and returns path starting
-    from this start node. It firstly selects a child of start, if this child
-    is DFF or Port, it returns. Else, make this child as start and recursively
-    search all children of this child.
-    """
-    path_behind.append(start)
-    for node in G[start]:
-        if node not in path_behind:
-            if isinstance(G.nodes[node]['property'], ta.DFF | ta.Port):
-                yield [start, node]
-            else:
-                for child in get_paths(G, node, path_behind):
-                    yield [start] + child
+def get_paths(G: nx.DiGraph, start) -> list:
+    def get_paths_recursive(G: nx.DiGraph, parent, path_nodes: list) -> list:
+        """A generator returns paths
+        
+        A recursive method which accepts a parent node and returns path starting
+        from this start node. It firstly selects a child of start, if this child
+        is DFF or Port, it returns. Else, make this child as start and 
+        recursively search all children of this child.
+        """
+        for child in G[parent]:
+            if child not in path_nodes:
+                path_nodes.append(child)
+                if isinstance(G.nodes[child]['property'], ta.DFF | ta.Port):
+                    yield path_nodes
+                else:
+                    for child_path in get_paths_recursive(G, child, path_nodes):
+                        yield child_path
+                path_nodes.pop(-1)
+
+    for path_nodes in get_paths_recursive(G, start, [start]):
+        yield path_nodes
